6d9d671a5d85a4fc2390d86a00a0bcc3864a2b25
jhaastrup <jhaastrup21@gmail.com>
2019-05-21 12:29:27 +0100

    updated connection string on mongodb

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 //const winston = require('winston');
 //require mongoose for DB connection
-const mongoose = require ('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
 
-
 //require('./startup/logging')();
-require('./startup/routes')(app);
+require("./startup/routes")(app);
 //require('./startup/db')();
-require('./startup/validation')();
-require('./startup/prod')(app); 
+require("./startup/validation")();
+require("./startup/prod")(app);
 
-mongoose.connect('mongodb://localhost/jasflix')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+mongoose
+  .connect("mongodb+srv://jastrup:haastrup002@joke-b1rpb.mongodb.net/jasflix", {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => {
+    console.error("Could not connect to MongoDB...");
+    console.error(err);
+  });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
